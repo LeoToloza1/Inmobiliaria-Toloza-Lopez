ac99123b654b651a3dc68da33fdf8dfef7adda3d
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-03-29 18:39:51 -0300

    MODIFICAICON MODELO INQUILINO

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -7,5 +7,5 @@ public class Inquilino
     public string? dni { get; set; }
     public string? email { get; set; }
     public string? telefono { get; set; }
-    public string? estado { get; set; }
+    public bool? borrado { get; set; }
 }
