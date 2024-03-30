11b303cf735463b5ce42c5e9ff4693e825486bad
Toloza  Leonel <leotoloza6@gmail.com>
2024-03-30 02:09:47 -0300

    entidades nuevas

---
+++ Models/Contrato.cs
@@ -0,0 +1,11 @@
+namespace inmobiliaria_Toloza_Lopez.Models;
+public class Contrato
+{
+    public int id { get; set; }
+    public int id_inquilino { get; set; }
+    public int id_inmueble { get; set; }
+    public DateOnly fecha_inicio { get; set; }
+    public DateOnly fecha_fin { get; set; }
+    public decimal incremento { get; set; }
+    public string? estado { get; set; }
+}
---
+++ Models/Pago.cs
@@ -0,0 +1,13 @@
+namespace inmobiliaria_Toloza_Lopez.Models;
+public class Pago
+{
+    public int id { get; set; }
+    public int id_contrato { get; set; }
+    public DateOnly fecha_pago { get; set; }
+    public decimal importe { get; set; }
+    public string? estado { get; set; }
+    public int numero_pago { get; set; }
+    public string? detalle { get; set; }
+    public string? concepto { get; set; }
+
+}
