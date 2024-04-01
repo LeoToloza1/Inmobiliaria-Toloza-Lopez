b840cc8ed44a8b2e9e613cbc31acf4b0b219f222
Toloza  Leonel <leotoloza6@gmail.com>
2024-04-01 17:57:15 -0300

    abm de inmueble -> andando

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -5,27 +5,53 @@ namespace inmobiliaria_Toloza_Lopez.Controllers;
 
 public class InmuebleController : Controller
 {
-    public IActionResult Index()
+  public IActionResult Index()
+  {
+    RepositorioInmueble rp = new RepositorioInmueble();
+    var lista = rp.GetInmuebles();
+    return View(lista);
+  }
+  [HttpGet]
+  public IActionResult Create()
+  {
+    ViewBag.tipoForm = "Nuevo Inmueble";
+    ViewBag.verboForm = "Nuevo";
+    return View("InmuebleFormulario");
+  }
+  [HttpGet]
+  public IActionResult Edit(int id)
+  {
+    RepositorioInmueble rp = new RepositorioInmueble();
+    var inmueble = rp.GetInmueble(id);
+    ViewBag.tipoForm = "Editando Inmueble";
+    ViewBag.verboForm = "Update";
+    return View("InmuebleFormulario", inmueble);
+  }
+  [HttpPost]
+  public IActionResult Nuevo(Inmueble inmueble)
+  {
+    RepositorioInmueble rp = new RepositorioInmueble();
+    if (ModelState.IsValid)
     {
-        RepositorioInmueble rp = new RepositorioInmueble();
-        var lista = rp.GetInmuebles();
-        return View(lista);
+      rp.GuardarInmueble(inmueble);
+      return RedirectToAction("Index");
     }
-      [HttpGet]
-    public IActionResult Create()
-    {
-        ViewBag.tipoForm = "Nuevo Inmueble";
-        return View("InmuebleFormulario");
-    }
-      [HttpGet]
-    public IActionResult Update(int id)
-    {
-        RepositorioInmueble rp = new RepositorioInmueble();
-        var inmueble = rp.GetInmueble(id);
-        ViewBag.tipoForm = "Editando Inmueble";
-        ViewBag.verboForm = "save";
-        return View("InmuebleFormulario", inmueble);
-    }
+    return View(inmueble);
+  }
+  [HttpPost]
+  public IActionResult Update(Inmueble inmueble)
+  {
+    RepositorioInmueble rp = new RepositorioInmueble();
+    rp.ActualizarInmueble(inmueble);
+    return RedirectToAction("Index");
+  }
+
+  public IActionResult Delete(int id)
+  {
+    RepositorioInmueble rp = new RepositorioInmueble();
+    rp.EliminarInmueble(id);
+    return RedirectToAction("Index");
+  }
 
 
 
