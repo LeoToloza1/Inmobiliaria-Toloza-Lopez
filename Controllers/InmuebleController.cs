29b1153d3900feee19187968bb5b42374565c95d
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-04-07 10:33:25 -0300

    maldito paginado

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -34,19 +34,22 @@ public class InmuebleController : Controller
   //   return View(lista);
   // }
   [HttpGet]
-  public IActionResult Index(int page = 1)
+  public IActionResult Index(int page = 1, string usoInmueble = "", string precioInmueble = "", string tipoInmueble = "", string ciudadInmueble = "", string zonaInmueble = "")
+  //int page, int pageSize, string usoInmueble = "", string precioInmueble = "", string tipoInmueble = "", string ciudadInmueble = "", string zonaInmueble = ""
   {
+
+    ViewBag.query = "usoinmueble " + usoInmueble + ", precioinmueble " + precioInmueble + ", tipoinmueble " + tipoInmueble + ", ciudadinmueble " + ciudadInmueble + ", zonainmueble " + zonaInmueble;
     ViewBag.tipoInmuebles = _repositorioTipoInmueble.GetTipoInmuebles();
     ViewBag.ciudades = _repositorioCiudad.ObtenerCiudades();
     ViewBag.zonas = _repositorioZona.ListarZonas();
-    int pageSize = 1; // Elementos por pagina
-    var totalItems = _repositorioInmueble.GetTotalInmuebles(); // Obtener el total de elementos
+    int pageSize = 3; // Elementos por pagina
+    var totalItems = _repositorioInmueble.GetTotalInmuebles(usoInmueble, precioInmueble, tipoInmueble, ciudadInmueble, zonaInmueble); // Obtener el total de elementos
     var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize); // Calcular el total de páginas
-    var inmuebles = _repositorioInmueble.GetInmuebles(page, pageSize); // Obtener los inmuebles para la página actual
+//    var inmuebles = _repositorioInmueble.GetInmuebles(page, pageSize); // Obtener los inmuebles para la página actual
+     var inmuebles = _repositorioInmueble.GetInmuebles(page, pageSize,usoInmueble, precioInmueble, tipoInmueble, ciudadInmueble, zonaInmueble); // Obtener los inmuebles para la página actual aplicando filtros
     //ViewBag.CurrentPage = page;
     ViewBag.paginaActual = page;
     ViewBag.TotalPages = totalPages;
-
     return View(inmuebles);
   }
   [HttpGet]
