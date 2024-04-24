69590ad2204d09b60d8ef1b5e0e74b02d5398dd3
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-04-24 10:36:01 -0300

    crete update delete tipo inmueble

--- repositorios/RepositorioTipoInmueble.cs
+++ repositorios/RepositorioTipoInmueble.cs
@@ -16,7 +16,7 @@ namespace inmobiliaria_Toloza_Lopez.Models
             var tipoInmuebles = new List<TipoInmueble>();
             using (var connection = new MySqlConnection(conexion))
             {
-                var sql = @$"SELECT {nameof(TipoInmueble.id)},{nameof(TipoInmueble.tipo)} FROM tipo_inmueble;";
+                var sql = @$"SELECT {nameof(TipoInmueble.id)},{nameof(TipoInmueble.tipo)},{nameof(TipoInmueble.borrado)} FROM tipo_inmueble;";
                 using (var command = new MySqlCommand(sql, connection))
                 {
                     connection.Open();
@@ -38,5 +38,86 @@ namespace inmobiliaria_Toloza_Lopez.Models
             return tipoInmuebles;
 
         }
+        public bool AltaTipoInmueble(TipoInmueble tipoInmueble)
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(conexion))
+                {
+                    var sql = @$"INSERT INTO tipo_inmueble({nameof(TipoInmueble.tipo)}) VALUES(@{nameof(TipoInmueble.tipo)});";
+                    using (var command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue($"@{nameof(TipoInmueble.tipo)}", tipoInmueble.tipo);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al insertar tipo de inmueble: {ex.Message}");
+                return false;
+            }
+        }
+        public bool UpdateTipoInmueble(TipoInmueble tipoInmueble)
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(conexion))
+                {
+                    var sql = @$"UPDATE `tipo_inmueble` SET
+                                    `({nameof(TipoInmueble.tipo)})` = VALUES(@{nameof(TipoInmueble.tipo)}
+                                    `({nameof(TipoInmueble.borrado)})` = VALUES(@{nameof(TipoInmueble.borrado)}
+                                WHERE `tipo_inmueble`.`id` = VALUES(@{nameof(TipoInmueble.id)};";
+                    using (var command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue($"@{nameof(TipoInmueble.tipo)}", tipoInmueble.tipo);
+                        command.Parameters.AddWithValue($"@{nameof(TipoInmueble.borrado)}", tipoInmueble.borrado);
+                        command.Parameters.AddWithValue($"@{nameof(TipoInmueble.id)}", tipoInmueble.id);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al insertar tipo de inmueble: {ex.Message}");
+                return false;
+            }
+        }
+public TipoInmueble GetTipoInmueble(int id)
+{
+    TipoInmueble tipoInmueble = null;
+    using (var connection = new MySqlConnection(conexion))
+    {
+        var sql = @$"SELECT {nameof(TipoInmueble.id)}, {nameof(TipoInmueble.tipo)}, {nameof(TipoInmueble.borrado)} FROM tipo_inmueble WHERE {nameof(TipoInmueble.id)} = @Id;";
+        using (var command = new MySqlCommand(sql, connection))
+        {
+            command.Parameters.AddWithValue("@Id", id);
+            connection.Open();
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    tipoInmueble = new TipoInmueble
+                    {
+                        id = reader.GetInt32(nameof(TipoInmueble.id)),                                
+                        tipo = reader.GetString(nameof(TipoInmueble.tipo)),
+                        borrado = reader.GetInt32(nameof(TipoInmueble.borrado))
+                    };
+                }
+            }
+            connection.Close();
+        }
+    }
+    return tipoInmueble;
+}
+
+ 
+
     }
 }
